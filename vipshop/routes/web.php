0860d7e1be0d754751dfd80cd49137d9696b0887
e <2250373930@qq.com>
2018-09-04 21:22:19 +0800

    添加控制器

--- vipshop/routes/web.php
+++ vipshop/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -14,3 +15,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// 后台路由
+     // 后台首页
+ Route::get('/admin','AdminController@index');
+      // 设置
+ Route::get('/admin/setting','AdminController@setting');
+ 	//商品
+ Route::resource('shop','ShopController');
+    //评论
+ Route::resource('pinglun','PinglunController');
+   //收货地址
+ Route::resource('address','AddressController');
+    //订单详情表
+ Route::resource('det','DetController');
+    //属性值表
+ Route::resource('vpro','VproController'); 
+    //属性表
+ Route::resource('pro','ProController');
+    // 商品评价
+  Route::resource('eva','EvaController');
+  //分类
+  Route::resource('cate','CateController');
+  // 购物车
+  Route::resource('scar','ScarController');
+   //友情链接
+  Route::resource('link','LinkController');
+  //登陆页面
+  Route::get('/admin/login', 'AdminController@login');
+  //登陆操作
+  Route::post('/admin/login', 'AdminController@dologin');
+
+  
+
